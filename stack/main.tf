7b8d17e164e668796e244a88ff1fcc91ee334ca3
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-22 01:13:08 +0000

    [WIP] Stack

---
+++ stack/main.tf
@@ -0,0 +1,24 @@
+resource "spacelift_stack" "stack" {
+    #REQUIRED
+    name                = var.name
+    repository          = var.repository
+    
+    #OPTIONAL
+    branch              = var.branch
+    space_id            = var.space_id
+    administrative      = var.administrative
+    autodeploy          = var.autodeploy
+    description         = var.description
+    project_root        = var.project_root
+    terraform_version   = var.terraform_version
+    labels              = var.labels
+    github_enterprise { 
+        namespace   = var.github_enterprise.namespace
+    }
+}
+
+resource "spacelift_context_attachment" "config" {
+  context_id = "config"
+  stack_id   = var.name
+  context_priority   = var.context_priority
+}
---
+++ stack/variable.tf
@@ -0,0 +1,72 @@
+# REQUIRED
+variable "name" {
+  description = "Name of the Spacelift stack."
+  type        = string
+}
+
+variable "repository" {
+  description = "Repository URL for the Spacelift stack."
+  type        = string
+}
+
+#OPTIONAL
+variable "space_id" {
+  description = "ID of the space where the stack is created."
+  type= string
+  default = "root"
+}
+
+variable "administrative" {
+  description = "Flag to indicate if the stack has administrative permissions."
+  type= bool
+  default= false
+}
+
+variable "autodeploy" {
+  description = "Flag to enable automatic deployment for the stack."
+  type= bool
+  default= true
+}
+
+variable "branch" {
+  description = "Git branch to use for the stack."
+  type= string
+  default= "main"
+}
+
+variable "description" {
+  description = "Description of the Spacelift stack."
+  type        = string
+  default     = null
+}
+
+variable "project_root" {
+  description = "Project root directory for the Spacelift stack."
+  type        = string
+  default     = "/"
+}
+
+variable "terraform_version" {
+  description = "Terraform version to use for the stack."
+  type        = string
+  default     = "1.5.7"
+}
+
+variable "labels" {
+  description = "List of labels to assign to the stack."
+  type        = list(string)
+  default     = ["all"]
+}
+
+variable "github_enterprise" {
+  description = "Configuration for GitHub Enterprise integration."
+  type = object({
+    namespace = optional(string, "nodadyoushutup-terraform")
+  })
+}
+
+variable "context_priority" {
+  description = "Priority of the context when attached to the stack."
+  type        = number
+  default     = 100
+}
