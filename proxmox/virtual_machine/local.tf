c538a3e417db2e0bdc67f70a64b7e6f46347b7f5
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-22 00:28:32 +0000

    [WIP] Spacelift

--- proxmox/virtual_machine/local.tf
+++ proxmox/virtual_machine/local.tf
@@ -1,6 +1,5 @@
 # modules/proxmox/virtual_machine/local.tf
 
 locals {
-  virtual_machine = try(yamldecode(file("/mnt/workspace/virtual_machine.yaml")), {})
-  provider = try(yamldecode(file("/mnt/workspace/provider.yaml")), {})
+  config = try(yamldecode(file("/mnt/workspace/config.yaml")), yamldecode(file("/mnt/workspace/config.yml")))
 }
