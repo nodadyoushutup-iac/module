9c15c6ad5ab5e81a3f3a2b958948f55aa2026a4b
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-22 02:05:04 +0000

    [WIP] Spacelift

--- spacelift/stack/main.tf
+++ spacelift/stack/main.tf
@@ -1,20 +1,23 @@
 resource "spacelift_stack" "stack" {
-    #REQUIRED
-    name                = var.name
-    repository          = var.repository
-    
-    #OPTIONAL
-    branch              = var.branch
-    space_id            = var.space_id
-    administrative      = var.administrative
-    autodeploy          = var.autodeploy
-    description         = var.description
-    project_root        = var.project_root
-    terraform_version   = var.terraform_version
-    labels              = var.labels
-    github_enterprise { 
-        namespace   = var.github_enterprise.namespace
-    }
+  ## REQUIRED ##
+  name                = var.name
+  repository          = var.repository
+
+  ## UNIQUE ##
+  description         = var.description
+  labels              = var.labels
+  project_root        = var.project_root
+  
+  ## OPTIONAL ##
+  branch              = var.branch
+  space_id            = var.space_id
+  administrative      = var.administrative
+  autodeploy          = var.autodeploy
+  terraform_version   = var.terraform_version
+  
+  github_enterprise { 
+    namespace   = var.github_enterprise.namespace
+  }
 }
 
 resource "spacelift_context_attachment" "config" {
