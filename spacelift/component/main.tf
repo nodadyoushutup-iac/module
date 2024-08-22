b71ac67efa3e5de66e74f255b6099cd797305b5d
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-22 04:59:01 +0000

    [WIP] Component

--- spacelift/component/main.tf
+++ spacelift/component/main.tf
@@ -84,6 +84,91 @@ module "infra" {
   }
 }
 
+module "init" {
+  source  = "spacelift.io/nodadyoushutup/stack/spacelift"
+  count = try(contains(local.config.component, var.component)) ? 1 : 0
+
+  # REQUIRED
+  name = try(local.stack.init.name, "${var.component}_init")
+  repository = try(local.stack.init.repository, var.component)
+
+  # UNIQUE
+  description = try(
+      try(
+          local.stack.init.description, 
+          local.config.global.stack.description
+      ), 
+      "${var.component} initialization"
+  )
+  project_root = try(
+      try(
+      local.stack.init.project_root, 
+      local.config.global.stack.project_root
+      ), 
+      "init"
+  )
+  labels = try(
+      try(
+      concat(local.stack.init.labels, ["init", var.component]), 
+      concat(local.config.global.stack.labels, ["init", var.component])
+      ),
+      ["init", var.component]
+  )
+
+  # OPTIONAL
+  space_id = try(
+      try(
+      local.stack.init.space_id, 
+      local.config.global.stack.space_id
+      ), 
+      null
+  )
+  administrative = try(
+      try(
+      local.stack.init.administrative, 
+      local.config.global.stack.administrative
+      ), 
+      null
+  )
+  autodeploy = try(
+      try(
+      local.stack.init.autodeploy, 
+      local.config.global.stack.autodeploy
+      ), 
+      null
+  )
+  branch = try(
+      try(
+      local.stack.init.branch, 
+      local.config.global.stack.branch
+      ), 
+      null
+  )
+  terraform_version = try(
+      try(
+      local.stack.init.terraform_version, 
+      local.config.global.stack.terraform_version
+      ), 
+      null
+  )
+  context_priority = try(
+      try(
+      local.stack.init.context_priority, 
+      local.config.global.stack.context_priority
+      ), 
+      null
+  )
+  github_enterprise = { 
+      namespace = try(
+      try(
+          local.stack.init.github_enterprise.namespace, 
+          local.config.global.stack.github_enterprise.namespace
+      ),
+      null
+      )
+  }
+}
+
 module "config" {
   source  = "spacelift.io/nodadyoushutup/stack/spacelift"
   count = try(contains(local.config.component, var.component)) ? 1 : 0
@@ -170,87 +255,3 @@ module "config" {
   depends_on = [module.infra]
 }
 
-module "init" {
-  source  = "spacelift.io/nodadyoushutup/stack/spacelift"
-  count = try(contains(local.config.component, var.component)) ? 1 : 0
-
-  # REQUIRED
-  name = try(local.stack.init.name, "${var.component}_init")
-  repository = try(local.stack.init.repository, var.component)
-
-  # UNIQUE
-  description = try(
-      try(
-          local.stack.init.description, 
-          local.config.global.stack.description
-      ), 
-      "${var.component} initialization"
-  )
-  project_root = try(
-      try(
-      local.stack.init.project_root, 
-      local.config.global.stack.project_root
-      ), 
-      "init"
-  )
-  labels = try(
-      try(
-      concat(local.stack.init.labels, ["init", var.component]), 
-      concat(local.config.global.stack.labels, ["init", var.component])
-      ),
-      ["init", var.component]
-  )
-
-  # OPTIONAL
-  space_id = try(
-      try(
-      local.stack.init.space_id, 
-      local.config.global.stack.space_id
-      ), 
-      null
-  )
-  administrative = try(
-      try(
-      local.stack.init.administrative, 
-      local.config.global.stack.administrative
-      ), 
-      null
-  )
-  autodeploy = try(
-      try(
-      local.stack.init.autodeploy, 
-      local.config.global.stack.autodeploy
-      ), 
-      null
-  )
-  branch = try(
-      try(
-      local.stack.init.branch, 
-      local.config.global.stack.branch
-      ), 
-      null
-  )
-  terraform_version = try(
-      try(
-      local.stack.init.terraform_version, 
-      local.config.global.stack.terraform_version
-      ), 
-      null
-  )
-  context_priority = try(
-      try(
-      local.stack.init.context_priority, 
-      local.config.global.stack.context_priority
-      ), 
-      null
-  )
-  github_enterprise = { 
-      namespace = try(
-      try(
-          local.stack.init.github_enterprise.namespace, 
-          local.config.global.stack.github_enterprise.namespace
-      ),
-      null
-      )
-  }
-}
