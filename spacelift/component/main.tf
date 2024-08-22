065acb9b2a7f6794f2cea03df83ad44ac33e00cb
nodadyoushutup <admin@nodadyoushutup.com>
2024-08-22 04:40:53 +0000

    [WIP] Spacelift

--- spacelift/component/main.tf
+++ spacelift/component/main.tf
@@ -84,88 +84,88 @@ module "infra" {
   }
 }
 
-module "config" {
-  source  = "spacelift.io/nodadyoushutup/stack/spacelift"
-  count = try(contains(local.config.component, var.component)) ? 1 : 0
+# module "config" {
+#   source  = "spacelift.io/nodadyoushutup/stack/spacelift"
+#   count = try(contains(local.config.component, var.component)) ? 1 : 0
 
-  # REQUIRED
-  name = "${var.component}_config"
-  repository = try(local.stack.config.repository, var.component)
+#   # REQUIRED
+#   name = "${var.component}_config"
+#   repository = try(local.stack.config.repository, var.component)
 
-  # UNIQUE
-  description = try(
-      try(
-          local.stack.config.description, 
-          local.config.global.stack.description
-      ), 
-      "${var.component} configuration"
-  )
-  project_root = try(
-      try(
-      local.stack.config.project_root, 
-      local.config.global.stack.project_root
-      ), 
-      "config"
-  )
-  labels = try(
-      try(
-      concat(local.stack.config.labels, ["config", var.component]), 
-      concat(local.config.global.stack.labels, ["config", var.component])
-      ),
-      ["config", var.component]
-  )
+#   # UNIQUE
+#   description = try(
+#       try(
+#           local.stack.config.description, 
+#           local.config.global.stack.description
+#       ), 
+#       "${var.component} configuration"
+#   )
+#   project_root = try(
+#       try(
+#       local.stack.config.project_root, 
+#       local.config.global.stack.project_root
+#       ), 
+#       "config"
+#   )
+#   labels = try(
+#       try(
+#       concat(local.stack.config.labels, ["config", var.component]), 
+#       concat(local.config.global.stack.labels, ["config", var.component])
+#       ),
+#       ["config", var.component]
+#   )
 
-  # OPTIONAL
-  space_id = try(
-      try(
-      local.stack.config.space_id, 
-      local.config.global.stack.space_id
-      ), 
-      null
-  )
-  administrative = try(
-      try(
-      local.stack.config.administrative, 
-      local.config.global.stack.administrative
-      ), 
-      null
-  )
-  autodeploy = try(
-      try(
-      local.stack.config.autodeploy, 
-      local.config.global.stack.autodeploy
-      ), 
-      null
-  )
-  branch = try(
-      try(
-      local.stack.config.branch, 
-      local.config.global.stack.branch
-      ), 
-      null
-  )
-  terraform_version = try(
-      try(
-      local.stack.config.terraform_version, 
-      local.config.global.stack.terraform_version
-      ), 
-      null
-  )
-  context_priority = try(
-      try(
-      local.stack.config.context_priority, 
-      local.config.global.stack.context_priority
-      ), 
-      null
-  )
-  github_enterprise = { 
-      namespace = try(
-      try(
-          local.stack.config.github_enterprise.namespace, 
-          local.config.global.stack.github_enterprise.namespace
-      ),
-      null
-      )
-  }
-  depends_on = [module.infra]
-}
+#   # OPTIONAL
+#   space_id = try(
+#       try(
+#       local.stack.config.space_id, 
+#       local.config.global.stack.space_id
+#       ), 
+#       null
+#   )
+#   administrative = try(
+#       try(
+#       local.stack.config.administrative, 
+#       local.config.global.stack.administrative
+#       ), 
+#       null
+#   )
+#   autodeploy = try(
+#       try(
+#       local.stack.config.autodeploy, 
+#       local.config.global.stack.autodeploy
+#       ), 
+#       null
+#   )
+#   branch = try(
+#       try(
+#       local.stack.config.branch, 
+#       local.config.global.stack.branch
+#       ), 
+#       null
+#   )
+#   terraform_version = try(
+#       try(
+#       local.stack.config.terraform_version, 
+#       local.config.global.stack.terraform_version
+#       ), 
+#       null
+#   )
+#   context_priority = try(
+#       try(
+#       local.stack.config.context_priority, 
+#       local.config.global.stack.context_priority
+#       ), 
+#       null
+#   )
+#   github_enterprise = { 
+#       namespace = try(
+#       try(
+#           local.stack.config.github_enterprise.namespace, 
+#           local.config.global.stack.github_enterprise.namespace
+#       ),
+#       null
+#       )
+#   }
+#   depends_on = [module.infra]
+# }
